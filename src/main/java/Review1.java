2f7096fb261ebbcaf0a34dce3aa342ed9e3bad3b
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-02-04 15:18:32 +0000

    Done

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,32 @@
 public class Review1 {
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int num = 9;
+		int num2 = 5;
+
+		System.out.println(num/num2);
+		System.out.println(num%num2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		boolean trueFalse = true;
+
+		System.out.println(trueFalse);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		String word = "Hello";
+		String word2 = "World";
+
+		System.out.println(word + " " + word2);
 	}
 
 }
